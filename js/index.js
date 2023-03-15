35ac0ea2c4613b0362efeecae2b106fb28f2f2f9
Philippe Chow <pak.chin.chow@student.ehb.be>
2023-03-15 12:03:33 +0100

    msg

--- js/index.js
+++ js/index.js
@@ -3,10 +3,36 @@
 const chat = {
     author: "yourName",
     init() {
+        this.fetchMessages
     },
     sendMessage() {
+
     },
     fetchMessages() {
+        fetch('https://dev2chat.onrender.com/messages')
+            .then(function (response) {
+                return response.json();
+            })
+
+            .then(function (json) {
+                console.log(json);
+                const msg = json[0];
+                console.log(msg.author);
+                console.log(msg.message);
+
+                let htmlString = `<div class="messageItem">
+                <div class="header">
+                    <span class="author">${msg.author}</span>
+                    <span class="time">00:00</span>
+                </div>
+                    <p>
+                        ${msg.message}
+                    </p>
+                </div>
+                `;
+
+                document.querySelector('#messageContainer').insertAdjacentHTML('beforeend',htmlString);
+            })
     },
     renderMessage(message) {
     }
